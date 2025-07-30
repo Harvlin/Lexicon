fe5856034023129004ef07a4c238fff02cf5514c
Harvlin Maximillian <harvlinmaximillian@gmail.com>
2025-07-30 07:44:31 +0700

    commit

--- backend/src/main/java/com/project/Lexicon/domain/dto/MessageResponse.java
+++ backend/src/main/java/com/project/Lexicon/domain/dto/MessageResponse.java
@@ -1,5 +1,12 @@
 package com.project.Lexicon.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MessageResponse {
     private String message;
 }
